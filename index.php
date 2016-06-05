05108d01701d4108817a6a606528e7c1ce41998e
threat777 <amadeus.seilert@gmail.com>
2016-06-05 18:08:21 -0300

    WS finalizado

--- index.php
+++ index.php
@@ -15,6 +15,26 @@ use Phalcon\Http\Response;
 $app = new Micro();
 $client = null;
 
+function convert_time($time) {
+
+    if ($time < 100) {
+        if ($time < 10)  {
+            $time = "0{$time}";
+        }
+        return "00:{$time}";
+    } else {
+        $hour = intval($time/100);
+        $minutes = $time % 100;
+        if ($hour < 10) {
+            $hour = "0{$hour}";
+        }
+        if ($minutes < 10) {
+            $minutes = "0{$minutes}";
+        }
+        return "{$hour}:{$minutes}";
+    }
+}
+
 try {
     $client = \Aws\DynamoDb\DynamoDbClient::factory(array(
         'region' => 'sa-east-1',
@@ -36,7 +56,7 @@ $app->get('/api/locations', function () use ($client) {
 
     try {
         $result = $client->scan([
-            'TableName' => 'RideLocations'
+            'TableName' => 'RideAlong_RideLocations'
         ]);
         $response->setStatusCode(200, "OK");
         $response->setContent(json_encode($result->get ( 'Items' )));
@@ -50,57 +70,74 @@ $app->get('/api/locations', function () use ($client) {
 });
 
 // Searches rides
-$app->get('/api/search/ride/{ride}', function ($ride) use ($client) {
+$app->get('/api/search/ride/{origin}/{destination}/{date}/{time}', function ($origin, $destination, $date, $time) use ($client) {
 
-    parse_str($ride, $data);
     $response = new Response();
-    echo $ride;
-    $origin = null;
-    $destination = null;
-    $date = null;
-    $time = null;
-//    try {
-//        $origin = $data['origin'];
-//        $destination = $data['destination'];
-//        $date = $data['date'];
-//        $time = $data['time'];
-//        $result = $client->query([
-//            'TableName' => 'Rides',
-//            'ConsistentRead' => true,
-//            'ProjectionExpression' => ''
-//            'Origin' => $origin,
-//            'Destination' => $destination,
-//            'Date' => $date,
-//            'Time' => $time
-//        ]);
-//
-//        $response->setStatusCode(200, "OK");
-//        $response->setContent(json_encode($result->get ( 'Items' )));
-//        return $response;
-//    } catch (DynamoDbException $e){
-//        $response->setStatusCode(400, "Bad Request");
-//        $response->setContent($e->getMessage());
-//        return $response;
-//    }
+
+    $date = str_replace("-", "/", $date);
+    $time = intval(str_replace(":", "", $time));
+
+    try {
+
+        $result = $client->scan([
+            'TableName' => 'RideAlong_Rides',
+            'ConsistentRead' => true,
+            'ProjectionExpression' => 'RideAlong_RideTime, RideAlong_RideDate, RideAlong_RideDriver, RideAlong_RideSlots, RideAlong_RideOrigin, RideAlong_RideDestination',
+            'Limit' => 20,
+            'FilterExpression' =>
+                                        'RideAlong_RideContext = :context_val AND
+                                        RideAlong_RideDate = :date_val AND
+                                        RideAlong_RideTime >= :time_start_val AND
+                                        RideAlong_RideTime <= :time_end_val AND
+                                        RideAlong_RideDestination = :destination_val AND
+                                        RideAlong_RideOrigin = :origin_val',
+            'ExpressionAttributeValues' =>  [
+                                                ':context_val' => ['S' => 'UNIFESP'],
+                                                ':origin_val' => ['S' => $origin],
+                                                ':destination_val' => ['S' => $destination],
+                                                ':date_val' => ['S' => $date],
+                                                ':time_start_val' => ['N' => ($time - 100)],
+                                                ':time_end_val' => ['N' => ($time + 100)]
+                                            ]
+        ]);
+
+        $result = $result->get('Items');
+
+        foreach ($result as $item => $value){
+            $result[$item]['RideAlong_RideTime']['N'] = convert_time($value['RideAlong_RideTime']['N']);
+        }
+
+        $response->setStatusCode(200, "OK");
+        $response->setContent(json_encode($result));
+        return $response;
+    } catch (DynamoDbException $e){
+        $response->setStatusCode(400, "Bad Request");
+        $response->setContent($e->getMessage());
+        return $response;
+    }
 });
 
 // Adds a new ride
 $app->post('/api/add/ride', function () use ($client, $app){
 
     $data = $app->request->getJsonRawBody();
+
     $response = new Response();
+
+    $data->time = intval(str_replace(":", "", $data->time));
 
     try {
         $result = $client->putItem([
-            'TableName' => 'Rides',
+            'TableName' => 'RideAlong_Rides',
             'Item' => [
-                'RidesId' => ['S' => uniqid()], // Primary Key
-                'Date' => ['S' => $data->date],
-                'Time' => ['S' => $data->time],
-                'Driver' => ['S' => $data->driver],
-                'Slots' => ['N' => $data->slots],
-                'Origin' => ['S' => $data->origin],
-                'Destination' => ['S' => $data->destination]
+                'RideAlong_RideContext' => ['S' => 'UNIFESP'], // Primary Context Key
+                'RideAlong_RideID' => ['S' => uniqid()],
+                'RideAlong_RideTime' => ['N' => $data->time],
+                'RideAlong_RideDate' => ['S' => $data->date],
+                'RideAlong_RideDriver' => ['S' => $data->driver],
+                'RideAlong_RideSlots' => ['N' => $data->slots],
+                'RideAlong_RideOrigin' => ['S' => $data->origin],
+                'RideAlong_RideDestination' => ['S' => $data->destination]
             ]
         ]);
 
